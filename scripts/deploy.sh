793aad0e8dbe90349dc6cdff6955a1217c742955
Wim Wauters <wauterw@gmail.com>
2023-12-24 13:19:39 +0100

    Seed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,7 +14,8 @@ git stash
 git pull origin main
 
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+# composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache
 php artisan clear-compiled
