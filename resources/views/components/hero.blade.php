b8fe10a5f580745426a9bff449d7ee0910ba27be
Wim Wauters <wauterw@gmail.com>
2023-12-27 14:58:02 +0100

    Layout frontend

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -26,7 +26,7 @@
                             </a>
                         </div>
                     </div>
-                    <h1 class="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">A better way to ship your projects</h1>
+                    <h1 class="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">A... better way to ship your projects</h1>
                     <p class="mt-6 text-lg leading-8 text-gray-600">Esse id magna consectetur fugiat non dolor in ad laboris magna laborum ea consequat. Nisi irure aliquip nisi adipisicing veniam voluptate id. In veniam incididunt ex veniam adipisicing sit.</p>
                     <div class="mt-10 flex items-center gap-x-6">
                         <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
