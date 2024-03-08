564ee918746ed888207a69e2db0d17c0f2f5580d
Dude-Tech <161693850+Dude-Tech@users.noreply.github.com>
2024-03-08 19:36:56 +0100

    New version

--- lang/en/users.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// lang/en/messages.php
-
-return [
-    'user' => 'Gebruiker',
-    'users' => 'Users',
-    'user_management' => 'User Management',
-];
--- lang/nl/users.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// lang/en/messages.php
-
-return [
-    'user' => 'Gebruiker',
-    'users' => 'Gebruikers',
-    'user_management' => 'Gebruikersbeheer',
-];
